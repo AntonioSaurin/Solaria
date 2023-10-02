7316e041a248e2923074ba9c11ea3e6ecece25fa
Marcelo <marcelovdias5@gmail.com>
2023-10-02 08:55:15 -0300

    troca de extensao

---
+++ app/views/aboutUs.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="style/aboutUs.css" rel="stylesheet" >
+<title> Sobre nós </title>
+</head>
+<body>
+    
+    <section class="position_img_aboutUs">
+
+    </section>
+
+      <h1 class="h1_aboutUs">Sobre nós</h1>
+      <h2 class="h2_aboutUS">Escrito por Gabriel de Campos Silva em 08.06.2023</h2>
+        <section class="blog-content">
+
+            <p>O projeto Solaria é uma iniciativa criada por estudantes atentos as realidades das instituições de acolhimento,
+            com o propósito de trazer luz e esperança aos corações que mais precisam.</p>
+
+            <p>
+            O projeto visa oferecer um serviço onde um doador e uma instituição podem aproveitar de forma conjunta, pois, com um clique é possível doar o que ela necessita, como: alimentos, roupas, brinquedos, entre outros. Além disso, é oferecido a localização do lar de acolhimento escolhido no mapa, para que você possa visitá-lo futuramente. Um sistema totalmente transparente, onde, sempre no final de um mês, é enviado em forma de PDF ao seu e-mail onde a sua contribuição foi encaminhada.
+            </p>
+
+            <p>
+            A Solaria tem como o principal objetivo de tornar esses abrigos institucionais mais visíveis na nossa sociedade, além de incentivar as pessoas a fazerem doações para esses locais, com o intuito de fazer com que outras pessoas façam o mesmo. Outro aspecto relevante é fornecer orientações aos visitantes sobre o processo de adoção das crianças acolhidas. Por meio do nosso site, os interessados poderão estabelecer contato direto com as instituições para iniciar o processo de adoção.
+            </p>
+                
+            <p>Com um cardápio repleto de sabores inigualáveis e um toque perspicaz de agiotagem, garantimos que sua experiência conosco seja memorável tanto em termos de sabor quanto de serviços financeiros.</p>
+                
+            <p>
+            Construa o futuro desses jovens conosco!
+            </p>
+                
+            <p>Atenciosamente,</p>
+            <p>Equipe Solaria.</p>
+        
+            </section>
+</body>
+</html>
