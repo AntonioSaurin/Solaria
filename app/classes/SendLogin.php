a0690665b8b208645ecbf3b5326034f27251d603
Sadow <antoniosaurin08@gmail.com>
2023-10-02 18:17:09 -0300

    continuação login

---
+++ app/classes/SendLogin.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\classes;
+
+require_once '../../vendor/autoload.php';
+
+class SendLogin {
+    public function login($typedData, $dbData) {
+        
+        if($teste = 0){
+            return 1;
+        }else {
+            return 0;
+        }
+
+    }
+}
+
--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,3 +1,16 @@
 <?php
+use app\classes\SendLogin;
+use app\models\User;
+
 require_once '../../vendor/autoload.php';
 
+$user = new User;
+
+$userAction = $user->find('email', $_POST['email']);
+
+$sendLogin = new SendLogin;
+
+$actionSendLogin = $sendLogin->login([
+    'typeEmail' -> $email,
+    'typedPassword' -> $password
+], $userAction);
