4876cff29f6dbdfd9afab4098d6d52e695914fa3
Marcelo <marcelovdias5@gmail.com>
2023-10-03 15:09:09 -0300

    atualização do css do sobre nos dnv

--- app/views/aboutUs.php
+++ app/views/aboutUs.php
@@ -13,34 +13,35 @@
     </section>
     <section class="h_aboutUs">
       <h1 class="h1_aboutUs">Sobre nós</h1>
-      <h2 class="h2_aboutUS">Escrito por Gabriel de Campos Salles em 08.06.2023</h2>
+      <h2 class="h2_aboutUS">Escrito por Gabriel de Campos Silva em 08.06.2023</h2>
     </section>
       <section class="blog_aboutUs">
           <section class="blog_content">
             <section class="blog_content_p">
-              <h3 class="h3_aboutUs"> Solaria </h3>
-              <p>O projeto Solaria é uma iniciativa criada por estudantes atentos as realidades das instituições de acolhimento,
-              com o propósito de trazer luz e esperança aos corações que mais precisam.</p>
+            <h3 class="h3_aboutUs">Solaria</h3>
+                <section class="blog_content_img">
+                  <img id="img_brand_aboutUs" src="app/style/img/Brand.png">
+                </section>
+                  <p>O projeto Solaria é uma iniciativa criada por estudantes atentos as realidades das instituições de acolhimento,
+                  com o propósito de trazer luz e esperança aos corações que mais precisam.</p>
 
-              <p>
-              O projeto visa oferecer um serviço onde um doador e uma instituição podem aproveitar de forma conjunta, pois, com um clique é possível doar o que ela necessita, como: alimentos, roupas, brinquedos, entre outros. Além disso, é oferecido a localização do lar de acolhimento escolhido no mapa, para que você possa visitá-lo futuramente. Um sistema totalmente transparente, onde, sempre no final de um mês, é enviado em forma de PDF ao seu e-mail onde a sua contribuição foi encaminhada.
-              </p>
+                  <p>
+                  O projeto visa oferecer um serviço onde um doador e uma instituição podem aproveitar de forma conjunta, pois, com um clique é possível doar o que ela necessita, como: alimentos, roupas, brinquedos, entre outros. Além disso, é oferecido a localização do lar de acolhimento escolhido no mapa, para que você possa visitá-lo futuramente. Um sistema totalmente transparente, onde, sempre no final de um mês, é enviado em forma de PDF ao seu e-mail onde a sua contribuição foi encaminhada.
+                  </p>
 
-              <p>
-              A Solaria tem como o principal objetivo de tornar esses abrigos institucionais mais visíveis na nossa sociedade, além de incentivar as pessoas a fazerem doações para esses locais, com o intuito de fazer com que outras pessoas façam o mesmo. Outro aspecto relevante é fornecer orientações aos visitantes sobre o processo de adoção das crianças acolhidas. Por meio do nosso site, os interessados poderão estabelecer contato direto com as instituições para iniciar o processo de adoção.
-              </p>
-                  
-              <p>Com um cardápio repleto de sabores inigualáveis e um toque perspicaz de agiotagem, garantimos que sua experiência conosco seja memorável tanto em termos de sabor quanto de serviços financeiros.</p>
-                  
-              <p>
-              Construa o futuro desses jovens conosco!
-              </p>
-                  
-              <p id="blog_p_content">Atenciosamente,</p>
-              <p>Equipe Solaria.</p>
-            </section>
+                  <p>
+                  A Solaria tem como o principal objetivo de tornar esses abrigos institucionais mais visíveis na nossa sociedade, além de incentivar as pessoas a fazerem doações para esses locais, com o intuito de fazer com que outras pessoas façam o mesmo. Outro aspecto relevante é fornecer orientações aos visitantes sobre o processo de adoção das crianças acolhidas. Por meio do nosso site, os interessados poderão estabelecer contato direto com as instituições para iniciar o processo de adoção.
+                  </p>
+                      
+                  <p>
+                  Construa o futuro desses jovens conosco!
+                  </p>
+                      
+                  <p id="blog_p_content">Atenciosamente,</p>
+                  <p>Equipe Solaria.</p>
           </section>
         </section>
+      </section>
       <?php
       ?>
 </body>
