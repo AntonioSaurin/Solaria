058a7d301d0b61262573e4ea4d61370044f9e712
Marcelo <marcelovdias5@gmail.com>
2023-10-04 13:03:56 -0300

    Criando a pagina de informações do usuario

---
+++ app/views/infoUser.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="app/style/css/infoUser.css" rel="stylesheet">
+    <title> Conta </title>
+</head>
+<body>
+    <?php
+        include_once
+    ?>
+
+</body>
+</html>
