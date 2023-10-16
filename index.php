b97f3c687f42bc218e82fcb67e65c191dd48ce5a
Jonas <jonathanpereiraperes75@gmail.com>
2023-10-16 09:48:21 -0300

    Alteração no Login/Cadastro

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ session_start();
 
 $routes = [
     '/' => 'views/home.php',
-    '/login' => 'views/login.html',
-    '/cadastro' => 'views/register.html',
+    '/login' => 'views/login.php',
+    '/cadastro' => 'views/register.php',
     '/sobre-nos' => 'views/aboutUs.php',
     '/cadastro/efetuar' => 'controllers/createUser.php',
     '/login/efetuar' => 'controllers/login.php',
