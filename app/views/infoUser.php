919e7d746a6c81aa0ce3ee0b321a518ff32565c0
Marcelo <marcelovdias5@gmail.com>
2023-10-18 15:21:35 -0300

    Atualização da pag de Informações do Usuario

--- app/views/infoUser.php
+++ app/views/infoUser.php
@@ -16,40 +16,40 @@
     </header>
 
 
-    <main class="info_account">
+    <main class="infoAccountUser">
         
-        <section class="info_account">
-            <section class="space_img_account">
-                <img src="app/style/img/images.jpg" id="img_account">
-                <section class="info_name_account">
-                    <label class="name_account"> André Varíola do Nascimento Souza </label>
+        <section class="infoAccount">
+            <section class="spaceImgAccount">
+                <img src="app/style/img/images.jpg" id="imgAccount">
+                <section class="infoNameAccount">
+                    <label class="nameAccount"> André Varíola do Nascimento Souza </label>
                 </section>
-                <label class="Cpf_account"> CPF: 287.778.748.62 </label>
-                <label class="Dtnasc_account"> Data de Nascimento: 05/06/1995 </label>
+                <label class="CpfAccount"> CPF: 287.778.748.62 </label>
+                <label class="DtNascAccount"> Data de Nascimento: 05/06/1995 </label>
             </section>
 
-            <section class="info_account_top">
-                <label id="label_account_geral"> Geral </label>
-                <label id="label_account_endereco"> Endereço </label>
+            <section class="infoAccountTop">
+                <section id="labelAccountGeral"> <label class="labelTitleTop"> Geral  </label> </section>
+                <section id="labelAccountEndereco"> <label class="labelTitleTop"> Endereço </label>  </section>
             </section>
 
-            <section class="info_account_space">
-                <section class="info_account_color_test">
+            <section class="space">
+                <section class="infoAccountTableGeral">
                     
-                    <label id="label_account_email"> Email: </label>
-                    <label class="box_email"> andre_varnasc@gmail.com </label>
+                    <label id="labelAccountEmail"> E-mail: </label>
+                    <label class="boxEmail"> Andre_varnasc@gmail.com </label>
                     
-                    <label id="label_account_telefone"> Telefone: </label>
-                    <label class="box_telefone"> +55 (11) 93212-9875</label>
+                    <label id="labelAccounTelefone"> Telefone: </label>
+                    <label class="boxTelefone"> +55 (11) 93212-9875 </label>
                     
-                    <label id="label_account_senha"> Senha: </label>
-                    <label class="box_senha"> THESENHA </label>
+                    <label id="labelAccountSenha"> Senha: </label>
+                    <section class="boxSenha"> <label class="AccountSenha"> THESENHA </label> </section>
 
-                    <label id="label_account_confsenha"> Confirma: </label>
-                    <label class="box_confsenha"> THESENHA </label>
+                    <label id="labelAccountConfsenha"> Confirma: </label>
+                    <label class="boxConfSenha"> THESENHA </label>
                 </section>
 
-                <section class="info_account_cep_test">
+                <section class="infoAccountTableEndereco">
                     <label class="info_account"> kjmn </label>
                 </section>
             </section>
