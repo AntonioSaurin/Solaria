0ede08ac19a88dbca93cbcfac989d54b9baa3112
Sadow <antoniosaurin08@gmail.com>
2023-10-31 18:54:10 -0300

    Exemplo de jQuery para validar cenipejoto

--- app/views/teste.php
+++ app/views/teste.php
@@ -1,104 +1,90 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app\style\css\registerInstituicao.css">
-    <script src="app/javaScript/jQuery.js"></script>
-    <script src="app/javaScript/jMask.js"></script>
-    <title>Cadastrar</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('input').blur('input', function () {
+                cnpj = $(this).val();
+
+                cnpj = cnpj.replace(/[^\d]+/g, '');
+
+                if (cnpj == '') {
+                    $(this).css("border-width", "4px")
+                    $(this).css("border-color", "#ff0000");
+
+                    return false;
+                }
+
+                if (cnpj.length != 14) {
+                    $(this).css("border-width", "4px")
+                    $(this).css("border-color", "#ff0000");
+
+                    return false;
+                }
+                // Elimina CNPJs invalidos conhecidos
+                if (cnpj == "00000000000000" ||
+                    cnpj == "11111111111111" ||
+                    cnpj == "22222222222222" ||
+                    cnpj == "33333333333333" ||
+                    cnpj == "44444444444444" ||
+                    cnpj == "55555555555555" ||
+                    cnpj == "66666666666666" ||
+                    cnpj == "77777777777777" ||
+                    cnpj == "88888888888888" ||
+                    cnpj == "99999999999999") {
+                    $(this).css("border-width", "4px")
+                    $(this).css("border-color", "#ff0000");
+                }
+                // Valida DVs
+                tamanho = cnpj.length - 2
+                numeros = cnpj.substring(0, tamanho);
+                digitos = cnpj.substring(tamanho);
+                soma = 0;
+                pos = tamanho - 7;
+                for (i = tamanho; i >= 1; i--) {
+                    soma += numeros.charAt(tamanho - i) * pos--;
+                    if (pos < 2)
+                        pos = 9;
+                }
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(0)) {
+                    $(this).css("border-width", "4px")
+                    $(this).css("border-color", "#ff0000");
+
+                    return false;
+                }
+
+                tamanho = tamanho + 1;
+                numeros = cnpj.substring(0, tamanho);
+                soma = 0;
+                pos = tamanho - 7;
+                for (i = tamanho; i >= 1; i--) {
+                    soma += numeros.charAt(tamanho - i) * pos--;
+                    if (pos < 2)
+                        pos = 9;
+                }
+                resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                if (resultado != digitos.charAt(1)) {
+                    $(this).css("border-width", "4px")
+                    $(this).css("border-color", "#ff0000");
+                    
+                    return false;
+                }
+
+                $(this).css("border-width", "1px")
+                $(this).css("border-color", "#000");
+            });
+        });
+    </script>
 </head>
 
 <body>
-    <header>
-        <?php
-            include('header.php');
-        ?>
-    </header>
-    <main>
-        <span id="gradient">
-            <section class="signupBox form" id="pt1">
-                <form action="cadastro/efetuar" method="post" name="Cadastro">
-                    <span class="table1">
-                        <h1>Cadastrar</h1><br>
-                        <label>Nome da Instituição:</label>
-                        <input type="text" class="signupBox" name="name" id="name" placeholder="Nome da Instituição"><br>
 
-                        <label>CNPJ:</label>
-                        <input type="text" class="signupBox" name="CNPJ" id="CNPJ" placeholder="CNPJ" maxlength="14"><br>
+    Name: <input type="text" name="fullname"><br>
+    Email: <input type="text" name="email">
 
-                        <label>Telefone:</label>
-                        <input type="text" class="signupBox" name="phone" id="phone" placeholder="Telefone"maxlength="12"><br>
-                      
-                    </span>
-
-                    <span class="table2">
-                        <label>Email:</label>
-                        <input type="text" class="signupBox" name="email" id="email" placeholder="Email"><br>
-
-                        <label>Senha:</label>
-                        <input type="password" class="signupBox" name="password" id="password" placeholder="Senha"><br>
-
-                        <label>Confirmar Senha:</label>
-                        <input type="password" class="signupBox" name="conPassword" id="conPassword" placeholder="Confirmar Senha"><br>
-
-                        <span name="next" class="nextButton" onclick=changeVisibleAdress()>Proximo</span>
-
-                        <p class="link">Você já tem uma conta? <a href="/login">Login</a></p>
-                    </span>
-
-            </section>
-
-            <section class="signupBox form" id="pt2">
-                    <span class="table1">
-                        <h1>Cadastrar Endereço</h1><br>
-                        <label>CEP:</label>
-                        <input type="text" class="signupBox" name="CEP" id="CEP" placeholder="00000-000"><br>
-
-                        <label>CNPJ:</label>
-                        <input type="text" class="signupBox" name="CNPJ" id="CNPJ" placeholder="CNPJ" maxlength="14"><br>
-
-                        <label>Telefone:</label>
-                        <input type="text" class="signupBox" name="phone" id="phone" placeholder="Telefone"maxlength="12"><br>
-                      
-                    </span>
-
-                    <span class="table2">
-                        <label>CEP:</label>
-                        <input type="text" class="signupBox" name="CEP" id="CEP" placeholder="00000-000"><br>
-
-                        <label>Senha:</label>
-                        <input type="password" class="signupBox" name="password" id="password" placeholder="Senha"><br>
-
-                        <label>Confirmar Senha:</label>
-                        <input type="password" class="signupBox" name="conPassword" id="conPassword" placeholder="Confirmar Senha"><br>
-
-                        <button type="submit" name="submit" class="registerButton">Cadastrar</button>
-
-                        <p class="link">Você já tem uma conta? <a href="/login">Login</a></p>
-                    </span>
-                </form>
-
-            </section>
-            
-            <img class="img" src="app/style/img/brandWithout.png">
-        </span>
-    </main>
-    <script>
-        $("#phone").mask("(00) 00000-0000");
-        $("#CPF").mask("000.000.000-00");
-        $("#CNPJ").mask("00.000.000/0000-00");
-
-        function changeVisibleAdress() {
-            var box1 = document.getElementById("pt1");
-            var box2 = document.getElementById("pt2");
-            
-            box1.style.display = "none";
-            box2.style.display = "inline";
-        }
-    </script>
 </body>
 
 </html>
