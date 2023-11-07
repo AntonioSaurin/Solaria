c8e828ca17f4a4cd7a436c20afe195ee5bf0e767
Sadow <antonio.saurin@etec.sp.gov.br>
2023-11-07 13:20:14 -0300

    progressão da verificação de inputs

--- app/views/registerInstituicao.php
+++ app/views/registerInstituicao.php
@@ -29,11 +29,12 @@
                             placeholder="Nome da Instituição"><br>
 
                         <p>Responsável:</p>
-                        <input type="text" class="signupBox" name="responsible" id="responsible"
+                        <input type="text" class="signupBox" name="director" id="director"
                             placeholder="Responsavél"><br>
 
                         <p>CPF do Responsavél:</p>
-                        <input type="text" class="signupBox" name="CPF" id="CPF" placeholder="CPF" maxlength="14"><br>
+                        <input type="text" class="signupBox" name="directorCPF" id="directorCPF" placeholder="CPF"
+                            maxlength="14"><br>
 
                     </span>
 
@@ -60,55 +61,54 @@
                 <h1>Cadastrar Endereço</h1><br>
                 <span class="table1">
                     <p>CEP:</p>
-                    <input type="text" class="signupBox" name="CEP1" id="CEP1" placeholder="00000-000"><br>
+                    <input type="text" class="signupBox" name="CEP" id="CEP" placeholder="00000-000"><br>
 
                     <p>Estado:</p>
                     <select class="signupBox" name="state" id="state">
-                        <option value="1"> AC </option>
-                        <option value="2"> Al </option>
-                        <option value="3"> AP </option>
-                        <option value="4"> AM </option>
-                        <option value="5"> BA </option>
-                        <option value="6"> CE </option>
-                        <option value="7"> DF </option>
-                        <option value="8"> ES </option>
-                        <option value="9"> GO </option>
-                        <option value="10"> MA </option>
-                        <option value="11"> MT </option>
-                        <option value="12"> MS </option>
-                        <option value="13"> MG </option>
-                        <option value="14"> PA </option>
-                        <option value="15"> PB </option>
-                        <option value="16"> PE </option>
-                        <option value="17"> PI </option>
-                        <option value="18"> RJ </option>
-                        <option value="19"> RN </option>
-                        <option value="20"> RS </option>
-                        <option value="21"> RO </option>
-                        <option value="22"> RR </option>
-                        <option value="23"> SC </option>
-                        <option value="24"> SP </option>
-                        <option value="25"> SE </option>
-                        <option value="26"> TO </option>
+                        <option value="AC"> AC </option>
+                        <option value="AL"> Al </option>
+                        <option value="AP"> AP </option>
+                        <option value="AM"> AM </option>
+                        <option value="BA"> BA </option>
+                        <option value="CE"> CE </option>
+                        <option value="DF"> DF </option>
+                        <option value="ES"> ES </option>
+                        <option value="GO"> GO </option>
+                        <option value="MA"> MA </option>
+                        <option value="MT"> MT </option>
+                        <option value="MS"> MS </option>
+                        <option value="MG"> MG </option>
+                        <option value="PA"> PA </option>
+                        <option value="PB"> PB </option>
+                        <option value="PE"> PE </option>
+                        <option value="PI"> PI </option>
+                        <option value="RJ"> RJ </option>
+                        <option value="RN"> RN </option>
+                        <option value="RS"> RS </option>
+                        <option value="RO"> RO </option>
+                        <option value="RR"> RR </option>
+                        <option value="SC"> SC </option>
+                        <option value="SP"> SP </option>
+                        <option value="SE"> SE </option>
+                        <option value="TO"> TO </option>
                     </select>
 
-                    <p>Telefone:</p>
-                    <input type="text" class="signupBox" name="phone2" id="phone2" placeholder="Telefone"
+                    <p>Cidade:</p>
+                    <input type="text" class="signupBox" name="city" id="city" placeholder="Telefone"
                         maxlength="12"><br>
 
                     <span class="backButton" id="text" onclick=changeVisibleInfos()>Voltar</span>
                 </span>
 
                 <span class="table2">
-                <p>Bairro:</p>
-                    <input type="text" class="signupBox" name="neighborhood" id="neighborhood" placeholder="Bairro"><br>
+                    <p>Bairro:</p>
+                    <input type="text" class="signupBox" name="district" id="district" placeholder="Bairro"><br>
 
                     <p>Numero:</p>
                     <input type="text" class="signupBox" name="number" id="number" placeholder="Numero"><br>
 
-                    <p>Logradouro:</p>
-                    <input type="password" class="signupBox" name="publicPlace" id="publicPlace"
-                        placeholder="Logradouro"><br>
+                    <p>Rua:</p>
+                    <input type="text" class="signupBox" name="street" id="street" placeholder="Rua"><br>
 
                     <span class="nextButton" id="text" onclick=changeVisibleOthers()>Próximo</span>
 
@@ -121,7 +121,7 @@
             <section class="signupBox form" id="pt3">
                 <h1>Outras Informações</h1><br>
                 <span class="table1">
-                   
+
 
                     <span class="backButton" id="text" onclick=changeVisibleAdress()>Voltar</span>
                 </span>
@@ -147,8 +147,9 @@
     </main>
     <script>
         $("#phone").mask("(00) 00000-0000");
-        $("#CPF").mask("000.000.000-00");
+        $("#directorCPF").mask("000.000.000-00");
         $("#CNPJ").mask("00.000.000/0000-00");
+        $("#CEP").mask("00000-000");
 
         function changeVisibleInfos() {
 
@@ -163,9 +164,9 @@
         }
 
         function changeVisibleAdress() {
-            var empty = verifyEmpty(nome, cnpj, phone, email, password, conPassword);
+            var empty = verifyEmptyInfos(nome, cnpj, phone, email, director, directorCPF);
 
-            if (empty != true) {
+            if (empty == true) {
                 var box1 = document.getElementById("pt1");
                 var box2 = document.getElementById("pt2");
                 var box3 = document.getElementById("pt3");
@@ -177,13 +178,18 @@
         }
 
         function changeVisibleOthers() {
-            var box1 = document.getElementById("pt1");
-            var box2 = document.getElementById("pt2");
-            var box3 = document.getElementById("pt3");
+            var empty = verifyEmptyAddres(cep, state, city, district, street, number);
 
-            box1.style.display = "none";
-            box2.style.display = "none";
-            box3.style.display = "inline";
+            if (empty == true) {
+                var box1 = document.getElementById("pt1");
+                var box2 = document.getElementById("pt2");
+                var box3 = document.getElementById("pt3");
+
+                box1.style.display = "none";
+                box2.style.display = "none";
+                box3.style.display = "inline";
+            }
+
         }
     </script>
 </body>
