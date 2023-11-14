6f6dd6fbc6bfbbaed9079b43f39c25547ec7ac7c
Jonas <jonathanpereiraperes75@gmail.com>
2023-11-14 13:23:46 -0300

    teste de validation imput

--- app/views/teste.php
+++ app/views/teste.php
@@ -2,107 +2,153 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="app/style/css/bootstrap.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Validation - Easy Tutorials YouTube Channel</title>
+  <script src="https://kit.fontawesome.com/c4254e24a8.js" crossorigin="anonymous"></script>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Poppins', sans-serif;
+    }
 
+    .container {
+      width: 100%;
+      height: 100vh;
+      background: #141a34;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .useful-links {
+      position: absolute;
+      top: 5%;
+      right: 5%;
+
+    }
+
+    .useful-links a {
+      display: block;
+      color: #fff;
+      text-decoration: none;
+    }
+
+    .container form {
+      width: 90%;
+      max-width: 500px;
+      padding: 50px 30px 20px;
+      background: #fff;
+      border-radius: 4px;
+      box-shadow: 0 4px 30px rgba(0, 0, 0, 0.5);
+      position: relative;
+    }
+
+    .fa-paper-plane {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background: #fff;
+      font-size: 26px;
+      padding: 20px;
+      border-radius: 50%;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    }
+
+    .input-group {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      margin: 10px 0;
+      position: relative;
+    }
+
+    .input-group label {
+      flex-basis: 28%;
+    }
+
+    .input-group input,
+    .input-group textarea {
+      flex-basis: 68%;
+      background: transparent;
+      border: 0;
+      outline: 0;
+      padding: 10px 0;
+      border-bottom: 1px solid #999;
+      color: #333;
+      font-size: 16px;
+    }
+
+    ::placeholder {
+      font-size: 14px;
+    }
+
+    form button {
+      background: #141a34;
+      color: #fff;
+      border-radius: 4px;
+      border: 1px solid rgba(255, 255, 255, 0.7);
+      padding: 10px 40px;
+      outline: 0;
+      cursor: pointer;
+      display: block;
+      margin: 30px auto 10px;
+    }
+
+    .input-group span {
+      position: absolute;
+      bottom: 12px;
+      right: 17px;
+      font-size: 14px;
+      color: #f00;
+    }
+
+    #submit-error {
+      color: #f00;
+    }
+  </style>
 </head>
 
 <body>
+  <div class="container">
+    <form>
+      <i class="fas fa-paper-plane"></i>
 
-<form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label for="validationCustom01" class="form-label">First name</label>
-    <input type="text" class="form-control" id="validationCustom01" value="Mark" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
-  <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Last name</label>
-    <input type="text" class="form-control" id="validationCustom02" value="Otto" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
-  </div>
-  <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Username</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
-      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-      <div class="invalid-feedback">
-        Please choose a username.
+      <div class="input-group">
+        <label>Full Name</label>
+        <input type="text" placeholder="Enter your name">
+        <span id="name-error">name is required</span>
       </div>
-    </div>
-  </div>
-  <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">City</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
-    <div class="invalid-feedback">
-      Please provide a valid city.
-    </div>
-  </div>
-  <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">State</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Choose...</option>
-      <option>...</option>
-    </select>
-    <div class="invalid-feedback">
-      Please select a valid state.
-    </div>
-  </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Please provide a valid zip.
-    </div>
-  </div>
-  <div class="col-12">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-      <label class="form-check-label" for="invalidCheck">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
-    </div>
-  </div>
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
-  </div>
-</form>
 
+      <div class="input-group">
+        <label>Phone No.</label>
+        <input type="tel" placeholder="123 456 7890">
+        <span id="phone-error">phone is required</span>
+      </div>
+
+      <div class="input-group">
+        <label>Email Id</label>
+        <input type="email" placeholder="Enter Email">
+        <span id="email-error">email is required</span>
+      </div>
+
+      <div class="input-group">
+        <label>Your Message</label>
+        <textarea rows="5" placeholder="Enter your message"></textarea>
+        <span id="message-error">message is required</span>
+      </div>
+
+      <button>Submit</button>
+      <span id="submit-error">please fix error</span>
+
+    </form>
+
+  </div>
     <script>
-      // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+      
     </script>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
