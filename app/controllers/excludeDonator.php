eaff3f2a373a06d2d74b224c367254b3e5555dba
Sadow <antoniosaurin08@gmail.com>
2023-11-22 19:34:47 -0300

    a

--- app/controllers/excludeDonator.php
+++ app/controllers/excludeDonator.php
@@ -1,9 +1,35 @@
 <?php
 use app\classes\Logged;
+use app\models\Donation;
+use app\models\Donator;
+use app\models\Message;
+use app\models\Phone;
+use app\models\Photo;
+use app\models\User;
+
 require_once 'vendor/autoload.php';
 
 (new Logged)->verify();
 
-if($logged != true) {
-    echo 'oi';
+if($logged == false) {
+    header('Location: /login');
 }
+
+$user = (new User)->fullFind($_SESSION['id']);
+
+if($user == false) {
+    echo '<script> alert("Usuario com problemas de cadastro!) </script>"';
+    header('Location: /infoUser');
+}
+
+(new Message)->delUserMessage($user['ID']);
+
+(new Donation)->delete('id', $user['ID']);
+
+(new Donator)->delete('id', $user['ID']);
+
+(new Phone)->delete('id', $user['userPhone']);
+
+(new Photo)->delete('id', $user['userPhoto']);
+
+$user->delete('id', $user['ID']);
