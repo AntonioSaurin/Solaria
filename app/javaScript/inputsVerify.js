bf4f5aaf7e4e46205bd6ac28f8272a8706daf5f5
Sadow <antoniosaurin08@gmail.com>
2023-11-23 22:48:56 -0300

    a

--- app/javaScript/inputsVerify.js
+++ app/javaScript/inputsVerify.js
@@ -5,7 +5,17 @@ var email = false;
 var password = false;
 var conPassword = false;
 var director = false;
-var directorCPF = false
+var directorCPF = false;
+
+var nameError = document.getElementById("name-error");
+var directorError = document.getElementById("director-error");
+var directorCPFError = document.getElementById("directorCPF-error");
+var CNPJError = document.getElementById("CNPJ-error");
+var emailError = document.getElementById("email-error");
+var phoneError = document.getElementById("phone-error");
+
+
+var nextError = document.getElementsByClassName("nextButton");
 
 $(document).ready(function () {
     $('#CNPJ').blur('input', function () {
@@ -266,3 +276,99 @@ $('#conPassword').blur('input', function () {
     conPassword = true;
     $(this).css("border-color", "#ccc");
 })
+
+function validationNameInstituition() {
+    var name = document.getElementById('name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = "Nome é necessário";
+        return false;
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "Inválido";
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validationDirector() {
+    var director = document.getElementById('director').value;
+
+    if (director.length == 0) {
+        directorError.innerHTML = "Name necesário";
+        return false;
+    }
+
+    if (!director.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        directorError.innerHTML = "Inválido";
+        return false;
+    }
+    directorError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validationDirectorCPF() {
+    var directorCPF = document.getElementById('directorCPF').value;
+
+    if (directorCPF.length == 0) {
+        directorCPFError.innerHTML = "CPF é necessário";
+        return false;
+    }
+
+    if (!directorCPF.match(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)) {
+        directorCPFError.innerHTML = "Inválido";
+        return false;
+    }
+    directorCPFError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validationCNPJ() {
+    var CNPJ = document.getElementById('CNPJ').value;
+
+    if (CNPJ.length == 0) {
+        CNPJError.innerHTML = "CNPJ é necessário";
+        return false;
+    }
+
+    if (!CNPJ.match(/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/)) {
+        CNPJError.innerHTML = "Inválido";
+        return false;
+    }
+    CNPJError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validationEmail() {
+    var email = document.getElementById('email').value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = "email necessário";
+        return false;
+    }
+
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "Email inválido";
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validationPhone() {
+    var phone = document.getElementById('phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = "Telefone necessário";
+        return false;
+    }
+
+    // if (!phone.match(/^(\(\d{2}\))?(\d{10})$/)) {
+    //     phoneError.innerHTML = "Inválido";
+    //     return false;
+    // }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
