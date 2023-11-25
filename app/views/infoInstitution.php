f24c336f26843798a941f916a0aaed3fc96b03fa
Marcelo <marcelovdias5@gmail.com>
2023-11-25 14:14:14 -0300

    atualização da pagina de info das instituições

--- app/views/infoInstitution.php
+++ app/views/infoInstitution.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="app/javaScript/jQuery.js"></script>
+    <script src="app/javaScript/jMask.js"></script>
+    <script src="app/javaScript/jmodal.js"></script>
     <link href="app/style/css/infoInstitution.css" rel="stylesheet">
     <title> Conta </title>
 </head>
@@ -11,106 +14,147 @@
 <body>
     <header>
         <?php
-                //include_once("header.php");
+        include_once("header.php");
         ?>
     </header>
 
+    <section class="centerModal">
+        <section id="myModal" class="modalContainer">
+            <section id="modal">
+                <section class="block">
+                    <section class="modalShow">
+                        <h3 class="h3modal"> Deseja excluir a sua conta? </h3>
 
-    <main class="infoAccountUser">
-        
+                        <button class="btnmodal" id="Excluir"> Excluir </button>
+                        <button id="btncancelmodal" class="close"
+                            onclick="document.getElementById('myModal').style.display='none'">Cancelar</button>
+                        
+                    </section>
+                </section>
+
+            </section>
+        </section>
+    </section>
+
+    <section class="center">
         <section class="infoAccount">
-            <section class="spaceImgAccount">
-                <img src="app/style/img/imgInstitutionAdotar.png" id="imgAccount">
-                <section class="infoNameAccount">
-                    <label class="nameAccount"> Instituição Adotar</label>
-                </section>
-                <label class="cnpjAccount"> CNPJ: 12.345.678/0001-00 </label>
-            </section>
-
-            
-            <?php
-                include_once("historicInstitution.php");
-            ?>
-
-            <section class="infoAccountTop">
-                <section id="labelAccountGeral"> <label class="labelTitleTop"> Geral </label> </section>
-                <section id="labelAccountEndereco"> <label class="labelTitleTop"> Endereço </label>  </section>
-            </section>
-
-            <section class="tablesFlex">
-                <section class="spaceTableGeral">
-                    <section class="infoAccountTableGeral">
-
-                        <label id="labelAccountEmail"> E-mail: </label>
-                        <input type="text"class="boxEmail" placeholder="insti_doac@gmail.com"></label>
-                        
-                        <label class="labelAccountGeral"> Telefone: </label>
-                        <input class="boxTelefone" placeholder="+55 (11) 4656-2270" name="phone"> </input>
-                        
-                        <label class="labelAccountGeral"> Senha: </label>
-                        <input type="password" class="boxSenha" placeholder="senhaparainstituicaohahaha"> </input>
-
-                        <label class="labelAccountGeral"> Confirma Senha: </label>
-                        <input type="password" class="boxConfSenha" placeholder="senhaparainstituicaohahaha"> </input>
-
-                        <section id="spaceBtnExcluir"> <button id="btnExcluir"> Excluir Conta </button> </section>
-                    </section>
-                </section>
-                <section class="spaceTableEndereco">
-                    <section class="infoAccountTableEndereco">
-                        
-                        <label id="labelAccountCEP"> CEP: </label>
-                        <input type="text" class="boxCEP" placeholder="07500-000" maxlength="9"></input>
-
-                        <label id="labelState"> Estado: </label>
-                        <select class="optionStates">
-                            <option value="1"> AC </option>
-                            <option value="2"> Al </option>
-                            <option value="3"> AP </option>
-                            <option value="4"> AM </option>
-                            <option value="5"> BA </option>
-                            <option value="6"> CE </option>
-                            <option value="7"> DF </option>
-                            <option value="8"> ES </option>
-                            <option value="9"> GO </option>
-                            <option value="10"> MA </option>
-                            <option value="11"> MT </option>
-                            <option value="12"> MS </option>
-                            <option value="13"> MG </option>
-                            <option value="14"> PA </option>
-                            <option value="15"> PB </option>
-                            <option value="16"> PE </option>
-                            <option value="17"> PI </option>
-                            <option value="18"> RJ </option>
-                            <option value="19"> RN </option>
-                            <option value="20"> RS </option>
-                            <option value="21"> RO </option>
-                            <option value="22"> RR </option>
-                            <option value="23"> SC </option>
-                            <option value="24"> SP </option>
-                            <option value="25"> SE </option>
-                            <option value="26"> TO </option>
-                        </select>
-
-                        <label id="labelCity"> Cidade: </label>
-                        <input type="text" class="boxCidade" placeholder="Santa Isabel"> </input>
-
-                        <label class="labelAccountEndereco"> Bairro: </label>
-                        <input type="text" class="boxBairro" placeholder="Parque São Benedito"> </input>
-
-                        <label id="labelNumber"> Número: </label>
-                        <input type="text" class="boxNum" placeholder="509" maxlength="4"> </input>
-
-                        <label class="labelAccountEndereco"> Logradouro: </label>
-                        <input type="text" class="boxLogradouro" placeholder="R. Ver. Luiz Benedito"> </input>
-
-                        <section class="spaceBtnSave"> <button id="btnSalvar"> Salvar </button> </section>
-                    </section>
+            <section class="userImg">
+                <img src="app/style/img/imgInstitutionAdotar.png" id="userImg">
+                <section class="organInfo">
+                    <h3 id="infoUsername"> Instituição Adotar </h3>
+                    <label class="basicInfo"> CNPJ: 12.345.678/0001-00 </label>
                 </section>
             </section>
-            <section class="spaceDown"> </section>
-            
-    </main>
+            <section class="userInfo">
+            </section>
+        </section>
+    </section>
+
+    <?php
+    /*include_once("historicUser.php");*/
+    ?>
+
+    <section class="center" id="modifyFooter">
+        <section class="borderForm">
+            <section class="spaceForm">
+                <form name="formEditar" class="formSave">
+                    <section class="organForm">
+                        <label class="infoForm"> Email: </label>
+                        <input type="text" class="inputEdit"> </input>
+                        <label class="infoForm"> Telefone: </label>
+                        <input type="text" name="phone" id="phone" maxlength="12" class="inputEdit"> </input>
+                        <label class="infoForm"> Senha: </label>
+                        <input type="password" class="inputEdit"> </input>
+                        <label class="infoForm"> Confirmar Senha: </label>
+                        <input type="password" class="inputEdit"> </input>
+                </form>
+                <button type="button" id="btnExcluir"
+                onclick="document.getElementById('myModal').style.display='block'"> Excluir Conta </input>
+            </section>
+
+            <form name="formEditar" class="formSave">
+                <section class="organForm">
+                    <label class="infoForm"> CEP: </label>
+                    <input type="text" class="inputCEP"> </input>
+                    <label class="infoFormState"> Estado: </label>
+                    <select class="signupBox" name="state" id="state">
+                        <option value="AC"> Acre </option>
+                        <option value="AL"> Alagoas </option>
+                        <option value="AP"> Amapá </option>
+                        <option value="AM"> Amazonas </option>
+                        <option value="BA"> Bahia </option>
+                        <option value="CE"> Ceará </option>
+                        <option value="DF"> Distrito Federal </option>
+                        <option value="ES"> Espírito Santo </option>
+                        <option value="GO"> Goiás </option>
+                        <option value="MA"> Maranhão </option>
+                        <option value="MT"> Mato Grosso </option>
+                        <option value="MS"> Mato Grosso do Sul </option>
+                        <option value="MG"> Minas Gerais </option>
+                        <option value="PA"> Pará </option>
+                        <option value="PB"> Paraíba </option>
+                        <option value="PR"> Paraná </option>
+                        <option value="PE"> Pernambuco </option>
+                        <option value="PI"> Piauí </option>
+                        <option value="RJ"> Rio de Janeiro </option>
+                        <option value="RN"> Rio Grande do Norte </option>
+                        <option value="RS"> Rio Grande do Sul </option>
+                        <option value="RO"> Rondônia </option>
+                        <option value="RR"> Roraima </option>
+                        <option value="SC"> Santa Catarina </option>
+                        <option value="SP"> São Paulo </option>
+                        <option value="SE"> Sergipe </option>
+                        <option value="TO"> Tocantins </option>
+                    </select>
+                    <label class="infoCity"> Cidade: </label>
+                    <input type="text" class="inputCity"> </input>
+                    <label class="infoForm"> Bairro: </label>
+                    <input type="text" class="inputEdit"> </input>
+                    <label class="infoNum"> Número: </label>
+                    <input type="number" class="inputNum"> </input>
+                    <label class="infoForm"> Logradouro: </label>
+                    <input type="text" class="inputLogradouro"> </input>
+
+                    <input type="submit" value="Salvar" name="Salvar" id="btnSalvar"> </input>
+            </form>
+        </section>
+    </section>
+    </section>
+    </section>
+
+    <script>
+        $("#phone").mask("+55 (00) 00000-0000");
+
+        window.onclick = function (event) {
+            var modal = document.getElementById('modalContainer');
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        var botao = document.getElementById("btnExcluir");
+        var sair = document.getElementById("btncancelmodal");
+        var body = document.body;
+
+        botao.addEventListener("click", function () {
+            body.classList.toggle("no-scroll");
+
+        })
+        
+        sair.addEventListener("click", function (){
+            body.classList.toggle("no-scroll");
+        }
+        );
+
+    </script>
+
+    <footer id="footerArea">
+        <?php
+        include_once("footer.php");
+        ?>
+    </footer>
+
+
 </body>
 
 </html>
