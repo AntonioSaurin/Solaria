13b7b2ecf1f987e049520b4294da351405a69971
Clara <tcc.solaria@gmail.com>
2023-11-26 22:16:24 -0300

    tentei arrumar registro

--- app/views/register.php
+++ app/views/register.php
@@ -21,58 +21,58 @@
         <span id="gradient">
             <section class="signupBox form" id="pt1">
                 <form action="cadastro/efetuar" method="post" name="Cadastro">
+                    <h1>Cadastrar</h1>
                     <span class="table1">
-                        <h1>Cadastrar</h1><br>
-                        <p>Nome Completo:</p>
-                        <input type="text" class="signupBox" name="name" id="name" placeholder="Nome" required><br>
-
-                        <p>Data de Nascimento:</p>
-                        <input type="text" class="signupBox" name="birthDate" id="birthDate"
-                            placeholder="Data de Nascimento" maxlength="8" required><br>
-
-                        <p>CPF:</p>
-                        <input type="text" class="signupBox" name="CPF" id="CPF" placeholder="CPF" maxlength="9"
-                            required><br>
-
-                        <p>Telefone:</p>
-                        <input type="phone" class="signupBox" name="phone" id="phone" placeholder="Telefone"
-                            maxlength="12" required><br>
+                        <section class="input-group">
+                            <p>Nome Completo:</p>
+                            <input type="text" class="signupBox" name="name" id="name" placeholder="Nome" required>
+                        </section>
+                        <section class="input-group">
+                            <p>Data de Nascimento:</p>
+                            <input type="text" class="signupBox" name="birthDate" id="birthDate" placeholder="Data de Nascimento" maxlength="8" required>
+                        </section>
+                        <section class="input-group">
+                            <p>CPF:</p>
+                            <input type="text" class="signupBox" name="CPF" id="CPF" placeholder="CPF" maxlength="9" required>
+                        </section>
+                        <section class="input-group">
+                            <p>Telefone:</p>
+                            <input type="phone" class="signupBox" name="phone" id="phone" placeholder="Telefone"
+                            maxlength="12" required>
+                        </section>
                     </span>
-
                     <span class="table2">
-
-
-                        <p>Email:</p>
-                        <input type="email" class="signupBox" name="email" id="email" placeholder="Email" required><br>
-
-                        <p>Senha:</p>
-                        <input type="password" class="signupBox" name="password" id="password" placeholder="Senha"
-                            required><br>
-
-                        <p>Confirmar Senha:</p>
-                        <input type="password" class="signupBox" name="conPassword" id="conPassword"
-                            placeholder="Confirmar Senha" required><br>
-
-                        <button type="submit" name="submit" class="registerButton">Cadastrar</button>
-
-                        <p>Você já tem uma conta? <a href="/login" id="btnLogin">Login</a></p>
+                        <section class="input-group">
+                            <p>Email:</p>
+                            <input type="email" class="signupBox" name="email" id="email" placeholder="Email" required>
+                        </section>
+                        <section class="input-group">
+                            <p>Senha:</p>
+                            <input type="password" class="signupBox" name="password" id="password" placeholder="Senha" required>
+                        </section>
+                        <section class="input-group">
+                            <p>Confirmar Senha:</p>
+                            <input type="password" class="signupBox" name="conPassword" id="conPassword" placeholder="Confirmar Senha" required>
+                        </section>
+                        <span name="next" class="nextButton" id="text" onclick="changeVisibleAdress()">Próximo</span>
+                        <p class="link text">Você já tem uma conta? <a href="/login" id="btnLogin">Login</a></p>
                     </span>
                 </form>
-
             </section>
 
             <section class="signupBox form" id="pt2">
-                <h1>Cadastrar Endereço</h1>
-                <span class="table1">
-                    <label>CEP:</label>
-                    <section class="input-group">
-                        <input type="text" class="signupBox" name="CEP" id="CEP" onkeyup="validationCEP()">
-                        <span id="CEP-error"></span>
-                    </section>
+                <h2>Cadastrar Endereço</h2>
+                <form action="cadastro/efetuar" method="post" name="Cadastro">
+                    <span class="table1">
+                        <label>CEP:</label>
+                        <section class="input-group">
+                            <input type="text" class="signupBox" name="CEP" id="CEP" onkeyup="validationCEP()">
+                            <span id="CEP-error"></span>
+                        </section>
 
-                    <label>Estado:</label>
-                    <section class=" input-group">
-                        <select class="signupBox" name="state" id="state">
+                        <label>Estado:</label>
+                        <section class="input-group">
+                            <select class="signupBox" name="state" id="state">
                             <option value="AC"> Acre </option>
                             <option value="AL"> Alagoas </option>
                             <option value="AP"> Amapá </option>
@@ -99,47 +99,38 @@
                             <option value="SC"> Santa Catarina </option>
                             <option value="SP"> São Paulo </option>
                             <option value="SE"> Sergipe </option>
-                            <option value="TO"> Tocantinsa </option>
+                            <option value="TO"> Tocantins </option>
                         </select>
                     </section>
+                        <label>Cidade:</label>
+                        <section class="input-group">
+                            <input type="text" class="signupBox" name="city" id="city" onkeyup="validationCity()">
+                            <span id="city-error"></span>
+                        </section>
+                    <span class="table2">
+                        <label>Bairro:</label>
+                        <section class="input-group">
+                            <input type="text" class="signupBox" name="district" id="district" onkeyup="validationDistrict()">
+                            <span id="district-error"></span>
+                        </section>
 
-                    <label>Cidade:</label>
-                    <section class="input-group">
-                        <input type="text" class="signupBox" name="city" id="city" onkeyup="validationCity()">
-                        <span id="city-error"></span>
-                    </section>
+                        <label>Numero:</label>
+                        <section class="input-group">
+                            <input type="text" class="signupBox" name="number" id="number" onkeyup="validationNumber()">
+                            <span id="number-error"></span>
+                        </section>
 
-                    <span class=" backButton" id="text" onclick=changeVisibleInfos()>Voltar
+                        <label>Rua:</label>
+                        <section class="input-group">
+                            <input type="text" class="signupBox" name="street" id="street" onkeyup="validationStreet()">
+                            <span id="street-error"></span>
+                        </section>
+
+                        <span class="nextButton" id="text" onclick="changeVisibleInfos()">Voltar</span>
+                        <button type="submit" name="submit" class="registerButton align" id="text">Cadastrar</button>
+                        <p class="link text">Você já tem uma conta? <a href="/login" id="btnLogin">Login</a></p>
                     </span>
-                </span>
-
-                <span class="table2">
-                    <label>Bairro:</label>
-                    <section class="input-group">
-                        <input type="text" class="signupBox" name="district" id="district"
-                            onkeyup="validationDistrict()">
-                        <span id="district-error"></span>
-                    </section>
-
-                    <label>Numero:</label>
-                    <section class=" input-group">
-                        <input type="text" class="signupBox" name="number" id="number" onkeyup="validationNumber()">
-                        <span id="number-error"></span>
-                    </section>
-
-                    <label>Rua:</label>
-                    <section class=" input-group">
-                        <input type="text" class="signupBox" name="street" id="street" onkeyup="validationStreet()">
-                        <span id="street-error"></span>
-                    </section>
-
-                    <span class="nextButton" id="text" onclick=changeVisibleOthers()>Próximo
-                    </span>
-
-                    <p class="link text">Você já tem uma conta? <a href="/login" id="btnLogin">Login</a></label>
-                </span>
-
-
+                </form>
             </section>
 
             <img class="img" src="app/style/img/brandWithout.png">
@@ -147,30 +138,23 @@
     </main>
     <script>
         $("#phone").mask("(00) 00000-0000");
-        $("#CPF").mask("000.000.000-00");
-        $("#birthDate").mask("00/00/0000");
         $("#CEP").mask("00000-000");
+        $("#CPF").mask("000.000.000-00");
 
         function changeVisibleInfos() {
-
             var box1 = document.getElementById("pt1");
             var box2 = document.getElementById("pt2");
 
             box1.style.display = "inline";
             box2.style.display = "none";
-
         }
 
         function changeVisibleAdress() {
+            var box1 = document.getElementById("pt1");
+            var box2 = document.getElementById("pt2");
 
-            if (empty == true) {
-                var box1 = document.getElementById("pt1");
-                var box2 = document.getElementById("pt2");
-
-                box1.style.display = "none";
-                box2.style.display = "inline";
-
-            }
+            box1.style.display = "none";
+            box2.style.display = "inline";
         }
     </script>
 </body>
