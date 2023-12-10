974db10eecfb4aac5cc10a6eac54383be23e5573
Sadow <antoniosaurin08@gmail.com>
2023-12-10 19:14:09 -0300

    a

--- app/views/howToAdopt.php
+++ app/views/howToAdopt.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app/style/css/howToAdopt.css">
+    <script src="app/javaScript/jQuery.js"></script>
     <title>Como Adotar</title>
 </head>
 <body>
