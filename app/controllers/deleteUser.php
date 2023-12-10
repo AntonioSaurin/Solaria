d4204a0aae2a805d6e90e4324a2c80a3a63e74c6
Clara <tcc.solaria@gmail.com>
2023-12-10 17:55:23 -0300

    teste

---
+++ app/controllers/deleteUser.php
@@ -0,0 +1,37 @@
+<?php
+use app\models\Donation;
+use app\models\Donator;
+use app\models\Institution;
+use app\models\Message;
+use app\models\Phone;
+use app\models\Photo;
+use app\models\User;
+
+require_once 'vendor/autoload.php';
+
+$institution = filter_input(INPUT_POST, 'institution', FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+$id = filter_input(INPUT_POST, 'idDelete', FILTER_SANITIZE_NUMBER_INT);
+
+$user = (new User)->find('ID', $id);
+
+(new Message)->delUserMessage($user['ID']);
+
+(new Donation)->delete('donator', $user['ID']);
+
+if($institution === true){
+    (new Institution)->delete('accountID', $user['ID']);
+}
+
+if($institution === false){
+    (new Donator)->delete('accountID', $user['ID']);
+}
+
+(new User)->delete('ID', $user['ID']);
+
+if ($user['userPhoto'] != 1) {
+    (new Photo)->delete('ID', $user['userPhoto']);
+}
+
+(new Phone)->delete('ID', $user['userPhone']);
+
+return true;
--- app/models/Institution.php
+++ app/models/Institution.php
@@ -10,16 +10,31 @@ class Institution extends Model
     {
         $credentials = require $_SERVER['DOCUMENT_ROOT'] . '\app\config\database.php';
 
-        $sql = "SELECT
-            institution.ID AS ID,
-            userAccount.userName AS userName,
-            institution.needs AS needs,
-            photo.photoPath AS photoPath,
-            city.city AS city,
-            state.acronym AS state
-            FROM  dbsolaria.institution
-            INNER JOIN userAccount ON institution.accountID = userAccount.id 
-            INNER JOIN adress ON userAccount.userAdress = adress.id
+        $sql = "SELECT 
+        userAccount.ID as ID,
+        userAccount.userName AS userName, 
+        userAccount.userEmail AS userEmail,
+        institution.CNPJ AS CNPJ,
+        institution.director AS director,
+        institution.directorCPF AS directorCPF,
+        institution.instagram AS instagram,
+        institution.facebook AS facebook,
+        institution.description AS description,
+        institution.needs AS needs,
+        institution.state AS stateInstitution,
+        phone.DDD AS DDD, 
+        phone.phoneNumber AS phoneNumber,
+        adress.CEP AS CEP, 
+        state.acronym AS state, 
+        city.city AS city,
+        adress.district AS district,
+        userAccount.userAdressNumber AS AdressNumber,
+        adress.street AS street,
+        photo.photoPath AS photoPath
+        FROM userAccount 
+            INNER JOIN institution ON institution.accountId = userAccount.id 
+            INNER JOIN adress ON userAccount.userAdress = adress.id  
+            INNER JOIN phone ON userAccount.userPhone = phone.id
             INNER JOIN photo ON userAccount.userPhoto = photo.id
             INNER JOIN city ON adress.city = city.id
             INNER JOIN state ON city.state = state.id
